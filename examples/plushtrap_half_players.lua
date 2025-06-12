510cfe2496df04833cf0540cff706251dd4f2f53
sergeant <90758027+s3rgeant@users.noreply.github.com>
2025-06-12 16:41:05 +1000

    Add files via upload

---
+++ examples/plushtrap_half_players.lua
@@ -0,0 +1,15 @@
+function plushtrap_half_players()
+    local playerCount = player.GetCount()
+    playerCount = playerCount / 2
+
+    local num = 0
+
+    for _, ply in player.Iterator() do
+        if !ply:Alive() or ply:Team() == TEAM_SPECTATOR then ply:Spawn()
+        giveKiller(ply, "pill_plushtrap", true)
+        num = num + 1
+
+        if num >= playerCount then break end
+    end
+end
+
