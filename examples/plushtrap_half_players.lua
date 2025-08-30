3d282de1960094d27215c2a405ad1d8a25eba143
sergeant <90758027+s3rgeant@users.noreply.github.com>
2025-08-30 17:30:25 +1000

    Update plushtrap_half_players.lua

--- examples/plushtrap_half_players.lua
+++ examples/plushtrap_half_players.lua
@@ -1,15 +1,16 @@
 function plushtrap_half_players()
-    local playerCount = player.GetCount()
-    playerCount = playerCount / 2
+	local players = player.GetAll()
+	table.Shuffle(players)
 
-    local num = 0
+	local halfCount = math.floor(#players / 2)
 
-    for _, ply in player.Iterator() do
-        if !ply:Alive() or ply:Team() == TEAM_SPECTATOR then ply:Spawn() end -- Возрождаем игроков, если они мёртвые
-        giveKiller(ply, "pill_plushtrap", true)
-        num = num + 1
-
-        if num >= playerCount then break end
-    end
+	for i = 1, halfCount do
+		local ply = players[i]
+		if IsValid(ply) then
+			giveKiller(ply, "pill_wbonnie2", true)
+			ply:ReturnToSpawn()
+		end
+	end
 end
+
 
