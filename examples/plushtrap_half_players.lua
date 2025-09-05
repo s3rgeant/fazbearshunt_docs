865d228ce068a6e445c602a8c5af5e8616b14622
sergeant <90758027+s3rgeant@users.noreply.github.com>
2025-09-05 17:26:21 +1000

    Update plushtrap_half_players.lua

--- examples/plushtrap_half_players.lua
+++ examples/plushtrap_half_players.lua
@@ -7,10 +7,11 @@ function plushtrap_half_players()
 	for i = 1, halfCount do
 		local ply = players[i]
 		if IsValid(ply) then
-			giveKiller(ply, "pill_wbonnie2", true)
+			giveKiller(ply, "pill_plushtrap", true)
 			ply:ReturnToSpawn()
 		end
 	end
 end
+
 
 
